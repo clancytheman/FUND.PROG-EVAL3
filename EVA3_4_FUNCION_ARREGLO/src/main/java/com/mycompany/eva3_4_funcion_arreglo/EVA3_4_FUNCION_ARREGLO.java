b0e6f10571ec043137dd27b2d6c79c8d5d68ed08
Diego Escarcega <143562608+clancytheman@users.noreply.github.com>
2023-11-27 17:52:12 -0600

    Add files via upload

---
+++ EVA3_4_FUNCION_ARREGLO/src/main/java/com/mycompany/eva3_4_funcion_arreglo/EVA3_4_FUNCION_ARREGLO.java
@@ -0,0 +1,23 @@
+package com.mycompany.eva3_4_funcion_arreglo;
+
+/**
+ *
+ * @author DIEGO ESCARCEGA
+ */
+public class EVA3_4_FUNCION_ARREGLO {
+
+    public static void main(String[] args) {
+        int[] arreglo;
+        arreglo = crearArreglo(10);
+        for(int valor: arreglo)
+            System.out.print("[ " + valor + " ]");
+    }
+    
+    public static int[] crearArreglo(int tama){
+        int[] arreglo = new int[tama];
+        for (int i = 0; i < arreglo.length; i++) {
+            arreglo[i] = (int)(Math.random()* 100);
+        }
+        return arreglo;
+    }
+}
---
+++ EVA3_5_REFERENCIA/src/main/java/com/mycompany/eva3_5_referencia/EVA3_5_REFERENCIA.java
@@ -0,0 +1,19 @@
+package com.mycompany.eva3_5_referencia;
+
+/**
+ *
+ * @author DIEGO ESCARCEGA
+ */
+public class EVA3_5_REFERENCIA {
+
+    public static void main(String[] args) {
+        int[] arreglo = new int[10];
+        System.out.println(arreglo);
+        /*
+         "[I@24d46ca6" <---- "[" Arreglo, "I" Entero, "@" En, "24d46ca6" Dirección en la RAM.
+        -----------------------------------------------------------------------------------
+        VALOR --> VALOR. MODIFICA COPIA.
+        REFERENCIA --> DIRECCIÓN. MODIFICA ORIGINAL.
+         */
+    }
+}
